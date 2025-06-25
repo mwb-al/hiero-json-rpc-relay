388ccab03390da8621dbe7fb08bb6e7961798afa
Michał Walczak <michal.walczak@arianelabs.com>
2025-06-25 14:07:17 +0200

    chore: update OpenRPC JSON updater config (#1837)
    Signed-off-by: Michał Walczak <michal.walczak@arianelabs.com>

--- scripts/openrpc-json-updater/config.js
+++ scripts/openrpc-json-updater/config.js
@@ -12,7 +12,7 @@ export const CUSTOM_FIELDS = [
   'eth_feeHistory.result.schema.properties.reward.title',
   'eth_getTransactionCount.summary',
   'eth_maxPriorityFeePerGas.result.schema.description',
-  'eth_getStorageAt.params.2.required',
+  'eth_sendRawTransaction.summary',
 ];
 
 export const DISCARDED_METHODS = ['engine_*'];
