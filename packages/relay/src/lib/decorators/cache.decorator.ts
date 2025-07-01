e915d2d4ad0c48e2681171b2623e17ff3a64c71f
konstantinabl <konstantina.blajukova@gmail.com>
2025-07-01 14:11:51 +0300

    fix: fixes caching for nested objects and adds tests (#3873)
    Signed-off-by: Konstantina Blazhukova <konstantina.blajukova@gmail.com>
    Signed-off-by: konstantinabl <konstantina.blajukova@gmail.com>
    Co-authored-by: Luis Mastrangelo <acuarica@users.noreply.github.com>

--- packages/relay/src/lib/decorators/cache.decorator.ts
+++ packages/relay/src/lib/decorators/cache.decorator.ts
@@ -26,13 +26,11 @@ interface CacheOptions {
   ttl?: number;
 }
 
-type IArgument = Record<string, any>;
-
 /**
  * Iterates through the provided 'params' array and checks if any argument in 'args' at the specified 'index'
  * matches one of the pipe-separated values in 'value'. If a match is found, caching should be skipped.
  *
- * @param args - The IArguments arguments object
+ * @param args - The arguments passed to the method in an array
  * @param params - An array of CacheSingleParam caching rules
  * @returns 'true' if any argument matches a rule and caching should be skipped; otherwise, 'false'.
  *
@@ -42,7 +40,7 @@ type IArgument = Record<string, any>;
  *     value: 'pending|safe'
  *   }]
  */
-const shouldSkipCachingForSingleParams = (args: IArgument[], params: CacheSingleParam[] = []): boolean => {
+const shouldSkipCachingForSingleParams = (args: unknown[], params: CacheSingleParam[] = []): boolean => {
   for (const item of params) {
     const values = item.value.split('|');
     if (values.indexOf(args[item.index]) > -1) {
@@ -65,7 +63,7 @@ const shouldSkipCachingForSingleParams = (args: IArgument[], params: CacheSingle
  * a list of field-based skip conditions and checks if any of the fields in the input argument match any of the provided
  * values (supports multiple values via pipe '|' separators).
  *
- * @param args - The function's arguments object (e.g., `IArguments`), where values are accessed by index.
+ * @param args - The function's arguments object, where values are accessed by index.
  * @param params - An array of `CacheNamedParams` defining which arguments and which fields to inspect.
  * @returns `true` if any field value matches a skip condition; otherwise, `false`.
  *
@@ -79,7 +77,7 @@ const shouldSkipCachingForSingleParams = (args: IArgument[], params: CacheSingle
  *     }],
  *   }]
  */
-const shouldSkipCachingForNamedParams = (args: IArgument[], params: CacheNamedParams[] = []): boolean => {
+const shouldSkipCachingForNamedParams = (args: unknown[], params: CacheNamedParams[] = []): boolean => {
   for (const { index, fields } of params) {
     const input = args[index];
 
@@ -91,7 +89,7 @@ const shouldSkipCachingForNamedParams = (args: IArgument[], params: CacheNamedPa
       // convert "latest|safe" to ["latest", "safe"]
       const allowedValues = value.split('|');
       // get the actual value from the input object
-      const actualValue = (input as IArgument)[key];
+      const actualValue = input[key];
 
       // if the actual value is one of the values that should skip caching, return true
       if (allowedValues.includes(actualValue)) {
@@ -112,20 +110,18 @@ const shouldSkipCachingForNamedParams = (args: IArgument[], params: CacheNamedPa
  * - Arguments of type `RequestDetails` are ignored in the key generation.
  *
  * @param methodName - The name of the method being cached.
- * @param args - The arguments passed to the method (typically from `IArguments`).
+ * @param args - The arguments passed to the method.
  * @returns A string that uniquely identifies the method call for caching purposes.
  *
  * @example
  *   generateCacheKey('getBlockByNumber', arguments); // should return getBlockByNumber_0x160c_false
  */
-const generateCacheKey = (methodName: string, args: IArgument[]) => {
+const generateCacheKey = (methodName: string, args: unknown[]) => {
   let cacheKey: string = methodName;
-  for (const [, value] of Object.entries(args)) {
-    if (value?.constructor?.name != 'RequestDetails') {
+  for (const value of args) {
+    if (!(value instanceof RequestDetails)) {
       if (value && typeof value === 'object') {
-        for (const [key, innerValue] of Object.entries(value)) {
-          cacheKey += `_${key}_${innerValue}`;
-        }
+        cacheKey += `_${JSON.stringify(value)}`;
         continue;
       }
 
@@ -137,17 +133,17 @@ const generateCacheKey = (methodName: string, args: IArgument[]) => {
 };
 
 /**
- * This utility is used to scan through the provided arguments (typically from `IArguments`)
+ * This utility is used to scan through the provided arguments.
  * and return the first value that is identified as an instance of `RequestDetails`.
  *
  * If no such instance is found, it returns a new `RequestDetails` object with empty defaults.
  *
- * @param args - The arguments object from a function (typically `IArguments`).
+ * @param args - The arguments from a function.
  * @returns The first found `RequestDetails` instance, or a new one with default values if none is found.
  */
-const extractRequestDetails = (args: IArgument): RequestDetails => {
+const extractRequestDetails = (args: unknown[]): RequestDetails => {
   for (const [, value] of Object.entries(args)) {
-    if (value?.constructor?.name === 'RequestDetails') {
+    if (value instanceof RequestDetails) {
       return value;
     }
   }
@@ -176,7 +172,7 @@ export function cache(cacheService: CacheService, options: CacheOptions = {}) {
   return function (_target: any, _propertyKey: string, descriptor: PropertyDescriptor) {
     const method = descriptor.value;
 
-    descriptor.value = async function (...args: IArgument[]) {
+    descriptor.value = async function (...args: unknown[]) {
       const requestDetails = extractRequestDetails(args);
       const cacheKey = generateCacheKey(method.name, args);
 
